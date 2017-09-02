745c55d07eb41f42d4bb59ea400c0a5e15dfcc8c
Andreas Fuchs <asf@boinkor.net>
2017-09-02 11:11:27 -0700

    Add benchmarks for threadsafely-wrapped impls
    Turns out there's about a 20ns overhead in single-threaded
    operation. Good to know!

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -3,12 +3,12 @@
 extern crate test;
 extern crate ratelimit_meter;
 
-use ratelimit_meter::{Allower, GCRA, Limiter, Decider};
+use ratelimit_meter::{Allower, GCRA, Threadsafe, Limiter, Decider};
 use std::time::{Instant, Duration};
 
 #[bench]
 fn bench_gcra(b: &mut test::Bencher) {
-    let mut gcra = Limiter::new().capacity(50).weight(1).build::<GCRA>();
+    let mut gcra = Limiter::new().capacity(50).weight(1).build::<GCRA>().unwrap();
     let now = Instant::now();
     let ms = Duration::from_millis(20);
     let mut i = 0;
@@ -20,6 +20,24 @@ fn bench_gcra(b: &mut test::Bencher) {
 
 #[bench]
 fn bench_allower(b: &mut test::Bencher) {
-    let mut allower = Limiter::new().capacity(50).weight(1).build::<Allower>();
+    let mut allower = Limiter::new().capacity(50).weight(1).build::<Allower>().unwrap();
+    b.iter(|| allower.check());
+}
+
+#[bench]
+fn bench_threadsafe_gcra(b: &mut test::Bencher) {
+    let mut gcra = Limiter::new().capacity(50).weight(1).build::<Threadsafe<GCRA>>().unwrap();
+    let now = Instant::now();
+    let ms = Duration::from_millis(20);
+    let mut i = 0;
+    b.iter(|| {
+        i += 1;
+        gcra.test_and_update(now + (ms * i))
+    });
+}
+
+#[bench]
+fn bench_threadsafe_allower(b: &mut test::Bencher) {
+    let mut allower = Limiter::new().capacity(50).weight(1).build::<Threadsafe<Allower>>().unwrap();
     b.iter(|| allower.check());
 }
