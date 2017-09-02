c0eca4dfe78266f9262b454ab8da2a7181a84eb3
Andreas Fuchs <asf@boinkor.net>
2017-09-02 09:53:49 -0700

    Pull tests out of lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -100,35 +100,3 @@ pub trait Decider {
         self.test_and_update(Instant::now())
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use {GCRA, Limiter, Decider, Decision};
-    use std::time::{Instant, Duration};
-
-    #[test]
-    fn accepts_first_cell() {
-        let mut gcra = Limiter::new().capacity(5).weight(1).build::<GCRA>().unwrap();
-        assert_eq!(Decision::Yes, gcra.check());
-    }
-    #[test]
-    fn rejects_too_many() {
-        let mut gcra = Limiter::new().capacity(1).weight(1).build::<GCRA>().unwrap();
-        let now = Instant::now();
-        gcra.test_and_update(now);
-        gcra.test_and_update(now);
-        assert_ne!(Decision::Yes, gcra.test_and_update(now));
-    }
-    #[test]
-    fn allows_after_interval() {
-        let mut gcra = Limiter::new().capacity(1).weight(1).build::<GCRA>().unwrap();
-        let now = Instant::now();
-        let ms = Duration::from_millis(1);
-        gcra.test_and_update(now);
-        gcra.test_and_update(now+ms*1);
-        gcra.test_and_update(now+ms*2);
-        // should be ok again in 1s:
-        let next = now + Duration::from_secs(1);
-        assert_eq!(Decision::Yes, gcra.test_and_update(next));
-    }
-}
