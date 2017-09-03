d99109124fefd4e4de17782606c2cd912a9567db
Andreas Fuchs <asf@boinkor.net>
2017-09-03 15:11:32 -0700

    Actually measure something in the multithreaded test, split it out
    Turns out that cloning a Bencher for background threads means those
    threads' results get lost. Running in the foreground also shows
    numbers that are necessarily different, so let's split this bench out
    to a different file.

---
+++ benches/multi_threaded.rs
@@ -0,0 +1,38 @@
+#![feature(test)]
+
+extern crate test;
+extern crate ratelimit_meter;
+
+use ratelimit_meter::{GCRA, Decider};
+use std::time::{Instant, Duration};
+use std::thread;
+
+
+#[bench]
+fn bench_20threads(b: &mut test::Bencher) {
+    let mut lim = GCRA::for_capacity(50).cell_weight(1).build_sync();
+    let now = Instant::now();
+    let ms = Duration::from_millis(20);
+    let mut children = vec![];
+
+    lim.check_at(now).unwrap();
+    for _i in 0..19 {
+        let mut lim = lim.clone();
+        let mut b = b.clone();
+        children.push(thread::spawn(move || {
+            let mut i = 0;
+            b.iter(|| {
+                i += 1;
+                lim.check_at(now + (ms * i)).unwrap();
+            });
+        }));
+    }
+    let mut i = 0;
+    b.iter(|| {
+        i += 1;
+        lim.check_at(now + (ms * i)).unwrap();
+    });
+    for child in children {
+        child.join().unwrap();
+    }
+}
--- benches/single_threaded.rs
+++ benches/single_threaded.rs
@@ -6,7 +6,6 @@ extern crate ratelimit_meter;
 use ratelimit_meter::{GCRA, Threadsafe, Decider};
 use ratelimit_meter::example_algorithms::Allower;
 use std::time::{Instant, Duration};
-use std::thread;
 
 #[bench]
 fn bench_gcra(b: &mut test::Bencher) {
@@ -43,29 +42,4 @@ fn bench_threadsafe_allower(b: &mut test::Bencher) {
     let allower_one = Allower::new();
     let mut threadsafe_allower = Threadsafe::new(allower_one);
     b.iter(|| threadsafe_allower.check());
-}
-
-// This one doesn't seem to actually do a thing & I can't quite figure out why /:
-#[bench]
-fn bench_multithreading_potentially_buggy(b: &mut test::Bencher) {
-    let mut lim = GCRA::for_capacity(50).cell_weight(1).build_sync();
-    let now = Instant::now();
-    let ms = Duration::from_millis(20);
-    let mut children = vec![];
-
-    lim.check_at(now).unwrap();
-    for _i in 0..20 {
-        let mut lim = lim.clone();
-        let mut b = b.clone();
-        children.push(thread::spawn(move || {
-            let mut i = 0;
-            b.iter(|| {
-                i += 1;
-                lim.check_at(now + (ms * i)).unwrap();
-            });
-        }));
-    }
-    for child in children {
-        child.join().unwrap();
-    }
 }
