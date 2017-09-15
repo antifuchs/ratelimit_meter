e6dd9d224e1d435537f958b49ae7a2c39e6e725d
Andreas Fuchs <asf@boinkor.net>
2017-09-14 23:01:50 -0700

    Don't do git checkout games as much

--- ci/run-benchmarks.sh
+++ ci/run-benchmarks.sh
@@ -10,14 +10,16 @@ set -x
 
 # Get comparison data from the master branch for PRs:
 if [[ "${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}" != "master" ]]; then
+    cargo bench > benches-variable
+
     REMOTE_URL="$(git config --get remote.origin.url)"
     cd "${TRAVIS_BUILD_DIR}/.."
-    git clone ${REMOTE_URL} "${TRAVIS_REPO_SLUG}-bench"
+    git clone "${REMOTE_URL}" "${TRAVIS_REPO_SLUG}-bench"
     cd  "${TRAVIS_REPO_SLUG}-bench"
 
     git checkout master
-    cargo bench > benches-control
-    git reset --hard "${TRAVIS_COMMIT}"
-    cargo bench > benches-variable
+    cargo bench > "${TRAVIS_BUILD_DIR}"/benches-control
+
+    cd "${TRAVIS_BUILD_DIR}"
     cargo benchcmp benches-control benches-variable
 fi
